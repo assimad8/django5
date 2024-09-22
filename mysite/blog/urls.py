36a3e2ecce1fb67833d6835088940c724f850c96
emad <assimad8@gmail.com>
2024-09-22 20:05:06 +0100

    add post list and post detaills views and tamplate

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -6,6 +6,6 @@ app_name = 'blog'
 urlpatterns = [
     # post views
     path('',post_list,name='post_list'),
-    path('<int:id>/',post_detail,name='post_detail'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',post_detail,name='post_detail'),
 ]
 
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -12,10 +12,16 @@ def post_list(request):
         {'posts':posts}
     )
 
-def post_detail(request,id):
-    post = get_object_or_404(Post,id=id,status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post):
+    post1 = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day)
     return render(
         request,
         'blog/post/detail.html',
-        {'post':post}
+        {'post': post1}
     )
